9fcb266176cbcd25b889236dade934d8669012c5
viveksingh2893 <viveksingh2893@gmail.com>
2022-10-12 13:10:02 +0530

    meds

--- src/components/prescription/pres.js
+++ src/components/prescription/pres.js
@@ -1,26 +1,95 @@
 import React from "react";
-import { Input } from 'antd';
-import '../../assets/css/pres.css'
+import { Input } from "antd";
+import "../../assets/css/pres.css";
 
-const Pres=()=>{
-    return(
-        <div className="form_head form_head_bg">
-            <div className="add_input">
-            <Input placeholder="Medicine" className='form_input'style={{width:'80%',textAlign:'left'}}/>
-            </div>
-            <div className="add_input">
-            <Input placeholder="Dose" className='form_input' style={{width:'80%',textAlign:'left'}}/>
-            </div>
-            <div className="add_input">
-                <Input className='form_schedule'  placeholder="M"/>
-                <Input  className='form_schedule'  placeholder="A"/>
-                <Input  className='form_schedule'  placeholder="E"/>
-            </div>
-            <div className="add_input">
-            <Input className='form_input' placeholder="Quantity" style={{width:'80%',textAlign:'center'}}/>
-            </div>
-        </div>
-    )
-}
+const Pres = (props) => {
+  const handleMed = (e, i) => {
+    const form = [...props.form];
+    form[i].med = e.target.value;
+    props.setForm(form);
+  };
+
+  const handleDose = (e, i) => {
+    const form = [...props.form];
+    form[i].dose = e.target.value;
+    props.setForm(form);
+  };
+
+  const handleTime = (e, i) => {
+    const form = [...props.form];
+    form[i].time = e.target.value;
+    props.setForm(form);
+  };
+
+  const handleFreq = (e, i) => {
+    const form = [...props.form];
+    form[i].freq = e.target.value;
+    props.setForm(form);
+  };
+
+  const handleDur = (e, i) => {
+    const form = [...props.form];
+    form[i].dur = e.target.value;
+    props.setForm(form);
+  };
+
+  const handleQuantity = (e, i) => {
+    const form = [...props.form];
+    form[i].quantity = e.target.value;
+    props.setForm(form);
+  };
+
+  return (
+    <div className="form_head form_head_bg">
+      <div className="add_input">
+        <Input
+          placeholder="Medicine"
+          className="form_input"
+          style={{ width: "80%", textAlign: "left" }}
+          value={props.med}
+          onChange={(e)=>handleMed(e, props.idx)}
+        />
+      </div>
+      <div className="add_input">
+        <Input
+          placeholder="Dose"
+          className="form_input"
+          style={{ width: "80%", textAlign: "left" }}
+          value={props.dose}
+          onChange={(e)=>handleDose(e, props.idx)}
+        />
+      </div>
+      <div className="add_input">
+        <Input
+          className="form_schedule"
+          placeholder="M"
+          value={props.time}
+          onChange={(e)=>handleTime(e, props.idx)}
+        />
+        <Input
+          className="form_schedule"
+          placeholder="A"
+          value={props.freq}
+          onChange={(e)=>handleFreq(e, props.idx)}
+        />
+        <Input
+          className="form_schedule"
+          placeholder="E"
+          value={props.dur}
+          onChange={(e)=>handleDur(e, props.idx)}
+        />
+      </div>
+      <div className="add_input">
+        <Input
+          className="form_input"
+          placeholder="Quantity"
+          style={{ width: "80%", textAlign: "center" }}
+          value={props.quantity}
+          onChange={(e)=>handleQuantity(e, props.idx)}
+        />
+      </div>
+    </div>
+  );
+};
 
 export default Pres;
--- src/screen/prescription.js
+++ src/screen/prescription.js
@@ -2,11 +2,41 @@ import React, { useEffect, useState } from "react";
 import Pres from "../components/prescription/pres";
 import "../assets/css/pres.css";
 import SvgIcon from "../assets/svgicon/svgicon";
-import { Input } from 'antd';
+import { Input } from "antd";
 import { calculateNewValue } from "@testing-library/user-event/dist/utils";
+import { useNavigate } from "react-router-dom";
 const { TextArea } = Input;
 
 const Prescription = () => {
+  const navigate = useNavigate();
+  const [form, setForm] = useState([
+    {
+      med: null,
+      dose: null,
+      time: null,
+      freq: null,
+      dur: null,
+      quantity: null,
+    },
+  ]);
+
+  const handleAddMeds = () => {
+    const forms = [...form];
+    forms.push({
+      med: null,
+      dose: null,
+      time: null,
+      freq: null,
+      dur: null,
+      quantity: null,
+    });
+    setForm(forms);
+  };
+
+  console.log("form", form);
+
+  const handleSubmit = () => {};
+
   return (
     <div
       style={{
@@ -14,42 +44,136 @@ const Prescription = () => {
         flexDirection: "column",
         alignItems: "center",
         // justifyContent: "center",
-       
       }}
     >
-      <div style={{ display: "flex", flexDirection: "row", width: "100%", marginBottom:'1vw' }}>
-        <div style={{ display: "flex", flexDirection: "column", width: "95%",padding:'1rem' }}>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          width: "100%",
+          marginBottom: "1vw",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            width: "95%",
+            padding: "1rem",
+          }}
+        >
           {/* <div style={{display:'flex', justifyContent:'center', background:'#30C74E',alignItems:'center',backgroundColor:'#D9D9D9'}}>
             <p className="pres-title"style={{fontSize:"1.2rem"}}>Destratum health</p>
           </div> */}
           <SvgIcon name="prescription" width="5vw" height="5vh" />
         </div>
-        <div style={{ display: "flex", alignItems: "flex-end",justifyContent:'center', width: "100%", }}>
-            <p className="pres-title" style={{fontSize:'1.5rem',fontWeight:"bold",color:'#000'}}>PRESCRIPTION</p>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "flex-end",
+            justifyContent: "center",
+            width: "100%",
+          }}
+        >
+          <p
+            className="pres-title"
+            style={{ fontSize: "1.5rem", fontWeight: "bold", color: "#000" }}
+          >
+            PRESCRIPTION
+          </p>
         </div>
-        <div style={{ display: "flex",flexDirection:'column',justifyContent:'center', alignItems: "center", width: "100%" }}>
-            <div >
-            <p className="pres-title" style={{fontSize:"1rem", color:'black',fontWeight:"bold",textAlign:"left"}}>Dr.N.Thoufick,M.D</p>
-            <p className="pres-title" style={{fontSize:"0.8rem", color:'black',textAlign:'left'}}>CONSULTANTS NEURO</p>
-            <p className="pres-title" style={{fontSize:"0.8rem", color:'black',textAlign:'left'}}>REGD.NO</p>
-            <p className="pres-title" style={{fontSize:"0.8rem", color:'black',textAlign:'left'}}>MOBILE NO:978542536</p>
-            </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            width: "100%",
+          }}
+        >
+          <div>
+            <p
+              className="pres-title"
+              style={{
+                fontSize: "1rem",
+                color: "black",
+                fontWeight: "bold",
+                textAlign: "left",
+              }}
+            >
+              Dr.N.Thoufick,M.D
+            </p>
+            <p
+              className="pres-title"
+              style={{ fontSize: "0.8rem", color: "black", textAlign: "left" }}
+            >
+              CONSULTANTS NEURO
+            </p>
+            <p
+              className="pres-title"
+              style={{ fontSize: "0.8rem", color: "black", textAlign: "left" }}
+            >
+              REGD.NO
+            </p>
+            <p
+              className="pres-title"
+              style={{ fontSize: "0.8rem", color: "black", textAlign: "left" }}
+            >
+              MOBILE NO:978542536
+            </p>
+          </div>
         </div>
       </div>
-      <div className=" form_head " >
+      <div className=" form_head ">
         <p className="pres-title">Medicine</p>
         <p className="pres-title">Dose</p>
         <p className="pres-title">Time- Freq- Dur</p>
         <p className="pres-title">Quantity</p>
       </div>
-      
-        <Pres />
-      <div className='form_head'style={{background:'#fff',justifyContent:'flex-end'}}> 
-      <button className="button"style={{background:'#30C74E',fontSize:'1rem'}}>
-        + Add 
-      </button>
+
+      {form.map((addmeds, idx) => {
+        return (
+          <div key={`${addmeds + "_" + idx}`}>
+            <Pres
+              idx={idx}
+              topic={addmeds.topic}
+              addmeds={addmeds}
+              med={addmeds.med}
+              dose={addmeds.dose}
+              time={addmeds.time}
+              freq={addmeds.freq}
+              dur={addmeds.dur}
+              quantity={addmeds.quantity}
+              form={form}
+              setForm={setForm}
+            />
+          </div>
+        );
+      })}
+
+      <div
+        className="form_head"
+        style={{ background: "#fff", justifyContent: "flex-end" }}
+      >
+        <button
+          className="button"
+          style={{ background: "#30C74E", fontSize: "1rem" }}
+          onClick={() => {
+            handleAddMeds();
+            // navigate(0);
+          }}
+        >
+          + Add
+        </button>
       </div>
-      <button className="button">
+
+      <button
+        className="button"
+        onClick={() => {
+          handleSubmit();
+          // navigate(0);
+        }}
+      >
         Submit
       </button>
     </div>
