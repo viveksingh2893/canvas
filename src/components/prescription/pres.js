5c171ade156bd689f5cccb0f112a4caf4459fd2f
viveksingh2893 <viveksingh2893@gmail.com>
2022-10-14 17:02:25 +0530

    delete button

--- src/components/prescription/pres.js
+++ src/components/prescription/pres.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Input } from "antd";
 import "../../assets/css/pres.css";
+import { DeleteFilled } from "@ant-design/icons";
 
 const Pres = (props) => {
   const handleMed = (e, i) => {
@@ -39,6 +40,12 @@ const Pres = (props) => {
     props.setForm(form);
   };
 
+  function handleRemovePres(i) {
+    const forms = [...props.form];
+    forms.splice(i, 1);
+    props.setForm(forms);
+  }
+
   return (
     <div className="form_head form_head_bg">
       <div className="add_input">
@@ -47,7 +54,7 @@ const Pres = (props) => {
           className="form_input"
           style={{ width: "80%", textAlign: "left" }}
           value={props.med}
-          onChange={(e)=>handleMed(e, props.idx)}
+          onChange={(e) => handleMed(e, props.idx)}
         />
       </div>
       <div className="add_input">
@@ -56,7 +63,7 @@ const Pres = (props) => {
           className="form_input"
           style={{ width: "80%", textAlign: "left" }}
           value={props.dose}
-          onChange={(e)=>handleDose(e, props.idx)}
+          onChange={(e) => handleDose(e, props.idx)}
         />
       </div>
       <div className="add_input">
@@ -64,19 +71,19 @@ const Pres = (props) => {
           className="form_schedule"
           placeholder="M"
           value={props.time}
-          onChange={(e)=>handleTime(e, props.idx)}
+          onChange={(e) => handleTime(e, props.idx)}
         />
         <Input
           className="form_schedule"
           placeholder="A"
           value={props.freq}
-          onChange={(e)=>handleFreq(e, props.idx)}
+          onChange={(e) => handleFreq(e, props.idx)}
         />
         <Input
           className="form_schedule"
           placeholder="E"
           value={props.dur}
-          onChange={(e)=>handleDur(e, props.idx)}
+          onChange={(e) => handleDur(e, props.idx)}
         />
       </div>
       <div className="add_input">
@@ -85,9 +92,35 @@ const Pres = (props) => {
           placeholder="Quantity"
           style={{ width: "80%", textAlign: "center" }}
           value={props.quantity}
-          onChange={(e)=>handleQuantity(e, props.idx)}
+          onChange={(e) => handleQuantity(e, props.idx)}
         />
       </div>
+      {props.form.length > 1?
+      <div style={{display:'flex', marginLeft:'1vw', alignItems:'center', justifyContent:'center'}}>
+      <DeleteFilled
+        style={{
+          display: "flex",
+          height: "3vw",
+          fontSize: "2vw",
+          color: "gray",
+        }}
+        onClick={() => handleRemovePres(props.idx)}
+      ></DeleteFilled>
+      </div>
+      :
+      <div style={{display:'flex',marginLeft:'1vw', alignItems:'center', justifyContent:'center'}}>
+      <DeleteFilled
+        style={{
+          display: "flex",
+          height: "3vw",
+          fontSize: "2vw",
+          color: "gray",
+          visibility:'hidden'
+        }}
+        onClick={() => handleRemovePres(props.idx)}
+      ></DeleteFilled>
+      </div>
+}
     </div>
   );
 };
